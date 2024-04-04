fb36f9c09ce04d7ee9ed3e27ede88c39d162a167
Aleksandra-zimoch <aleksandrazimoch557@gmail.com>
2024-04-04 17:27:21 +0200

    Components Button, Item, Expenses, Income create

--- frontend/src/Components/Expenses/Expenses.js
+++ frontend/src/Components/Expenses/Expenses.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useGlobalContext } from "../../context/globalContext";
+import ExpenseForm from "./ExpenseForm";
+import IncomeItem from "../Item/Item";
 
 function Expenses() {
+  const { expenses, getExpenses, deleteExpense, totalExpenses } =
+    useGlobalContext();
+
+  useEffect(() => {
+    getExpenses();
+  }, []);
+
   return (
     <div>
       <h1>Wydatki</h1>
+      <div>
+        <h2>
+          Wydatki łącznie: <span>{totalExpenses()} PLN</span>
+        </h2>
+      </div>
+      <div>
+        <div>
+          <ExpenseForm />
+        </div>
+        <div>
+          {expenses.map((income) => {
+            const { _id, title, amount, date, category, description, type } =
+              income;
+            return (
+              <IncomeItem
+                key={_id}
+                id={_id}
+                title={title}
+                description={description}
+                amount={amount}
+                date={date}
+                type={type}
+                category={category}
+                deleteItem={deleteExpense}
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
